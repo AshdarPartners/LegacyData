5c2306d94cf658c08407a5829b8621d44388835b
Darin Strait <darin.strait@ashdar-partners.com>
2024-12-14 15:05:58 -0500

    General improvement for flexibilty.

--- Tests/Get-LegacyDataTestValue.ps1
+++ Tests/Get-LegacyDataTestValue.ps1
@@ -5,71 +5,42 @@ Retrieves values to use in a test configuration. Ex: server names, paths to data
 .NOTES
 THis should not be used to source credentials. See Get-LegacyDataCredential.ps1 for that.
 
-The (vauge) idea is that this file will decide where to get test values from. Those sources might be:
+This pulls values the locations of "test data" from a JSON file and puts in a hash for easy access.
+
+You can override the values used for the SQL OLEDB and SQL (Server) Client tests with environment variables.
+
+The (vague) idea is that this file will decide where to get test values from. Those sources might be:
 1. A JSON file in this folder (which is the current implementation)
-2. The environmentment ($env:)
+2. The environment ($env:)
 3. A JSON file in a different location
 
 #>
 [CmdletBinding()]
-param()
+param(
+    [string] $ConfigurationFilePath
+)
 
-$Path = $(Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition))
+if (-not $ConfigurationFilePath) {
 
-$Path = Join-Path -Path $path -Child "Tests"
+    $Path = $(Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition))
 
-$ConfigurationFile = Join-Path -Path $path -Child "LegacyData.TestValues.json"
+    $Path = Join-Path -Path $path -Child "Tests"
 
-$TestConfiguration = Get-Content -Path $ConfigurationFile -Raw |
+    $ConfigurationFilePath = Join-Path -Path $path -Child "LegacyData.TestValues.json"
+
+}
+
+$TestConfiguration = Get-Content -Path $ConfigurationFilePath -Raw |
     ConvertFrom-Json
 
-Write-Verbose -Message "Read Test values from '$ConfigurationFile'"
+if ($env:LegacyDataSqlInstanceName) {
+    $TestConfiguration.SqlOleDbHostName = $env:LegacyDataSqlInstanceName
+}
+
+if ($env:LegacyDataSqlDatabaseName) {
+    $TestConfiguration.SqlOleDbDatabaseName = $env:LegacyDataSqlDatabaseName
+}
+
+Write-Verbose -Message "Read Test values from '$ConfigurationFilePath'"
+
 $TestConfiguration
-
-<#
-
-I'm leaving this hear as an untested example of what we might do, it's more for the idea than a good
-example of an implementation
-
-
-$FileDirectory = "${env:\userprofile}"
-$FileName = 'LegacyData.Tests.cred'
-
-$LocalFile = Join-Path -path $FileDirectory -child $FileName
-
-$ProjectFile = Join-Path -path $FileDirectory -child $FileName
-
-
-if (Test-Path $LocalFile) {
-    Write-Verbose "$LocalFile found."
-    $UseThisFile = $LocalFile
-
-}
-elseif (Test-Path $ProjectFile )") {
-    Write-Verbose "$ProjectFile found."
-    $UseThisFile = $ProjectFile
-}
-else {
-    # Pull things from $env:
-
-    # the trick with this is that it has to match what comes out of the JSON file
-
-    $TestConfigurationEnvironment = @{
-        SqlInstance = $env:SqlInstanceName
-        SqlDatabase = $env:SqlDatabase
-        # and so forth.
-
-    }
-}
-
-if ($TestConfigurationEnvironment) {
-    $TestConfigurationEnvironment
-}
-else {
-    $TestConfiguration = Get-Content -Path $UseThisFile -Raw |
-        ConvertFrom-Json
-    $TestConfiguration
-}
-
-
-#>
